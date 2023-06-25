43205673e6f0d61a80dcdabca8e3be63cdc7386e
CJN4HUM <onahon.github@gmail.com>
2023-06-26 02:34:15 +0800

    Adding Logout Function and minor bug fixes.

--- script.js
+++ script.js
@@ -1,45 +1,41 @@
-
 const loginForm = document.getElementById('loginForm');
 const loginUsername = document.getElementById('username');
 const loginPassword = document.getElementById('loginPassword');
 
 loginForm.addEventListener('submit', function(event) {
-  event.preventDefault();
-  
-  const username = loginUsername.value;
-  const password = loginPassword.value;
+    event.preventDefault();
+    
+    const username = loginUsername.value;
+    const password = loginPassword.value;
 
-  const storedUsers = JSON.parse(localStorage.getItem('users')) || [];
+    const key = JSON.parse(localStorage.getItem('users'));
 
-  const matchedUser = storedUsers.find(user => user.username === username && user.password === password);
-
-  if (username === '') {
-    alert('Please fill out the username field.');
-  } else if (password === '') {
-    alert('Please fill out the password field.');
-  } else if (matchedUser) {
-    alert('Login successful!');
-    // Perform any necessary action after successful login
-    // For example, redirect to a new page
-    window.location.href = 'home-admin.html';
-  } else {
-    alert('Invalid username or password');
-  }
+    if (username === '') {
+        alert('Please fill out the username field.');
+    } else if (password === '') {
+        alert('Please fill out the password field.');
+    } else if (key.username == username && key.password == password) {
+        alert('Login successful!');
+        window.location.href = 'member-page.html';
+    } else {
+        alert('Invalid username or password');
+    }
 });
+
 
 const registerLink = document.getElementById('registerLink');
 const loginLink = document.getElementById('loginLink');
 
 registerLink.addEventListener('click', function(event) {
-  event.preventDefault();
-  loginForm.style.display = 'none';
-  registrationForm.style.display = 'block';
+    event.preventDefault();
+    loginForm.style.visibility = 'hidden';
+    registrationForm.style.visibility = 'visible';
 });
 
 loginLink.addEventListener('click', function(event) {
-  event.preventDefault();
-  loginForm.style.display = 'block';
-  registrationForm.style.display = 'none';
+    event.preventDefault();
+    loginForm.style.visibility = 'visible';
+    registrationForm.style.visibility = 'hidden';
 });
 
 const registrationForm = document.getElementById('registrationForm');
@@ -48,32 +44,30 @@ const regEmail = document.getElementById('regEmail');
 const regPassword = document.getElementById('regPassword');
 
 registrationForm.addEventListener('submit', function(event) {
-  event.preventDefault();
+    event.preventDefault();
 
-  const username = regUsername.value;
-  const email = regEmail.value;
-  const password = regPassword.value;
+    const username = regUsername.value;
+    const email = regEmail.value;
+    const password = regPassword.value;
 
-  if (username === '') {
-    alert('Please fill out the username field.');
-  } else if (email === '') {
-    alert('Please fill out the email field.');
-  } else if (password === '') {
-    alert('Please fill out the password field.');
-  } else {
-    const newUser = {
-      username: username,
-      email: email,
-      password: password
-    };
+    if (username === '') {
+        alert('Please fill out the username field.');
+    } else if (email === '') {
+        alert('Please fill out the email field.');
+    } else if (password === '') {
+        alert('Please fill out the password field.');
+    } else {
+        const newUser = {
+            username: username,
+            email: email,
+            password: password
+        };
 
-    const storedUsers = JSON.parse(localStorage.getItem('users')) || [];
-    storedUsers.push(newUser);
-    localStorage.setItem('users', JSON.stringify(storedUsers));
-    
-    alert('Registration successful!');
+        localStorage.setItem('users', JSON.stringify(newUser));
+        
+        alert('Registration successful!');
 
-    loginForm.style.display = 'block';
-    registrationForm.style.display = 'none';
-  }
+        loginForm.style.visibility = 'visible';
+        registrationForm.style.visibility = 'hidden';
+    }
 });
